ea8659f0e869d4ed222e57219f452282b7c77f87
Adrii4 <adrian44munoz@gmail.com>
2021-02-12 13:34:47 +0100

    Reto2

--- Javascript/Juegos.js
+++ Javascript/Juegos.js
@@ -1,3 +1,105 @@
+//PIEDRA PAPEL TIJERA
+//VARIABLES
+var suE= Math.floor(Math.random()*3)+1;
+var vic=0;
+var i;
+
+
+//METODOS
+function empezar(){
+    ocultarMenu();
+    document.getElementById("ppt").style.display="block";
+    document.getElementById("puntos").style.display="block";
+    document.getElementById("puntos").innerHTML="Puntos: "+vic;
+}
+
+function ocultarMenu(){
+    document.getElementById("logo").style.display="none";
+    document.getElementById("modo").style.display="none";
+    document.getElementById("go").style.display="none";
+}
+
+function empate(){
+    document.getElementById("versusp").innerHTML="EMPATE";
+    document.getElementById("versusp").style.color="#00e5ff";
+    document.getElementById("PPT").style.backgroundColor="blue";
+}
+function ganar(){
+    document.getElementById("versusp").innerHTML="VICTORIA";
+    document.getElementById("versusp").style.color="#22f5cb";
+    document.getElementById("PPT").style.backgroundColor="green";
+    vic++;
+    document.getElementById("puntos").innerHTML="Puntos: "+vic;
+}
+function perder(){
+    document.getElementById("versusp").innerHTML="DERROTA";
+    document.getElementById("versusp").style.color="#27032e";
+    document.getElementById("PPT").style.backgroundColor="red";
+}
+
+function terminarJuego(){
+    document.getElementById("logo").style.display="block";
+    document.getElementById("modo").style.display="block";
+    document.getElementById("una").style.display="block";
+    document.getElementById("UNA").style.display="block";
+    document.getElementById("tres").style.display="block";
+    document.getElementById("TRES").style.display="block";
+    document.getElementById("versus").style.display="none";
+}
+
+function quienGana(a){
+    document.getElementById("ppt").style.display="none";
+    document.getElementById("versus").style.display="block";
+    document.getElementById("versus").style.display="block";
+    if(suE==a){
+        empate();
+    }else if(suE==3 && a==2){
+        perder();
+    }else if(suE==3 && a==1){
+        ganar();
+    }else if(suE==2 && a==3){
+        ganar();
+    }else if(suE==2 && a==1){
+        perder();
+    }else if(suE==1 && a==3){
+        perder();
+    }else if(suE==1 && a==2){
+           ganar();
+    }
+    if(suE==1){
+        document.getElementById("suE").src="Imagenes/PiedraE.png";
+    } else if(suE==2){
+        document.getElementById("suE").src="Imagenes/PapelE.png"
+    } else{
+        document.getElementById("suE").src="Imagenes/TijeraE.png"
+    }
+    if(a==1){
+        document.getElementById("tuE").src="Imagenes/Piedra.png";
+    } else if(a==2){
+        document.getElementById("tuE").src="Imagenes/Papel.png"
+    } else{
+        document.getElementById("tuE").src="Imagenes/Tijera.png"     
+    }
+    jugarVeces--;
+    if(jugarVeces==3 && vic==2 || jugarVeces==1 && vic==1){
+        document.getElementById("otra").src="Imagenes/victoria.png";
+        terminar=1;
+    }
+}
+
+function vj(){
+    if(terminar==1){
+        terminarJuego();
+    }
+    document.getElementById("versus").style.display="none";
+    document.getElementById("ppt").style.display="block";
+    document.getElementById("otra").style.display="none";
+}
+
+
+
+
+//MATAPATOS
 //CONSTANTES
 const PATO_NORMAL=1000;
 const PATO_ESPECIAL=5000;
@@ -37,21 +139,21 @@ function CargaHard(){
 	document.getElementById("modoHard").style.display="none";
 	document.getElementById("modoFancy").style.display="none";
 	document.getElementById("seleccion").innerHTML="Modo #~·#";
-	document.getElementById("pato1").src="img/Joan.png";
-	document.getElementById("pato2").src="img/Joan.png";
-	document.getElementById("pato3").src="img/Joan.png";
-	document.getElementById("pato4").src="img/Joan.png";
-	document.getElementById("pato5").src="img/Joan.png";
-	document.getElementById("pato6").src="img/Joan.png";
-	document.getElementById("pato7").src="img/Joan.png";
-	document.getElementById("pato8").src="img/Joan.png";
-	document.getElementById("pato9").src="img/Joan.png";
-	document.getElementById("pato10").src="img/JoanEspecial.png";
-	document.getElementById("pato11").src="img/JoanEspecial.png";
-	document.getElementById("pato12").src="img/JoanEspecial.png";
-	document.getElementById("pato13").src="img/JoanEspecial.png";
-	document.getElementById("pato14").src="img/JoanNegro.png";
-	document.getElementById("pato15").src="img/JoanNegro.png";
+	document.getElementById("pato1").src="Imagenes/Joan.png";
+	document.getElementById("pato2").src="Imagenes/Joan.png";
+	document.getElementById("pato3").src="Imagenes/Joan.png";
+	document.getElementById("pato4").src="Imagenes/Joan.png";
+	document.getElementById("pato5").src="Imagenes/Joan.png";
+	document.getElementById("pato6").src="Imagenes/Joan.png";
+	document.getElementById("pato7").src="Imagenes/Joan.png";
+	document.getElementById("pato8").src="Imagenes/Joan.png";
+	document.getElementById("pato9").src="Imagenes/Joan.png";
+	document.getElementById("pato10").src="Imagenes/JoanEspecial.png";
+	document.getElementById("pato11").src="Imagenes/JoanEspecial.png";
+	document.getElementById("pato12").src="Imagenes/JoanEspecial.png";
+	document.getElementById("pato13").src="Imagenes/JoanEspecial.png";
+	document.getElementById("pato14").src="Imagenes/JoanNegro.png";
+	document.getElementById("pato15").src="Imagenes/JoanNegro.png";
 	document.getElementById("juego").style.backgroundColor="brown";
 	tiempomili=500;
 	tiempomilireloj=200;
@@ -140,7 +242,7 @@ function disparar(){
 		recargar();
 	} else{
 	balas -= 1;
-	document.getElementById("juego").style.cursor="url(img/Disparo.png) 16 16, auto";
+	document.getElementById("juego").style.cursor="url(Imagenes/Disparo.png) 16 16, auto";
 	document.getElementById("disparo").volume=0.1;
 	document.getElementById("disparo").play();
 	document.getElementById("Balas").innerHTML="Balas: "+balas;
@@ -150,7 +252,7 @@ function disparar(){
 
 function recargar(){
 	balas=6;
-	document.getElementById("juego").style.cursor="url(img/Recargando.png) 16 16, auto";
+	document.getElementById("juego").style.cursor="url(Imagenes/Recargando.png) 16 16, auto";
 	document.getElementById("Balas").innerHTML="Balas: "+balas;
 	document.getElementById("recargar").volume=1;
 	document.getElementById("recargar").play();
@@ -158,7 +260,7 @@ function recargar(){
 }
 
 function volverMira(){
-	document.getElementById("juego").style.cursor="url(img/Mira.png) 16 16, auto";
+	document.getElementById("juego").style.cursor="url(Imagenes/Mira.png) 16 16, auto";
 }
 
 function volverFondo(){
@@ -191,11 +293,11 @@ function volarPatos(){
 function Volumen(){
 	
 	if(mute==1){
-		document.getElementById("BMute").src="img/Muteado.png";
+		document.getElementById("BMute").src="Imagenes/Muteado.png";
 		document.getElementById("cancion").volume=0;
 		mute=0;
 	} else if (mute==0){
-		document.getElementById("BMute").src="img/Desmuteado.png";
+		document.getElementById("BMute").src="Imagenes/Desmuteado.png";
 		document.getElementById("cancion").volume=0.05;
 		document.getElementById("cancion").play();
 		mute=1;
