b4bb48ec19ea4cb29a1b53f3391559fa95e2d1a3
Adrii4 <adrian44munoz@gmail.com>
2021-02-13 14:08:44 +0100

    Reto2

--- Javascript/Juego.js
+++ Javascript/Juego.js
@@ -1,9 +1,10 @@
-//CONSTANTES
+//PUNTOS DE LOS PATOS AL ACERTAR
 const PATO_NORMAL=1000;
 const PATO_ESPECIAL=5000;
 const PATO_NEGRO=1000;
 
 //VARIABLES
+//una array de patos en la que cada uno tiene una animación y van a ser activados de forma aleatoria
 let patos = ["pato1","pato2","pato3","pato4","pato5","pato6","pato7",
 			"pato8","pato9","pato10","pato11","pato12","pato13",
 			"pato14","pato15"];
@@ -18,20 +19,21 @@ var cantPatos=10;
 var Reloj=25;
 var mute=0;
 
-//METODOS
+//FUNCIONES
+//Una pequeña pantalla de carga de 3 segundos que te carga el modo normal o "NOVATO" para que estes listo
 function Carga(){	
 	document.getElementById("modoNormal").style.display="none";
 	document.getElementById("modoHard").style.display="none";
 	document.getElementById("modoFancy").style.display="none";
 	document.getElementById("seleccion").innerHTML="Modo Novato";
-	if(i==0){
+	if(i==0){ //es un pequeño reloj que tarda 500 milisegundos en pasar al siguiente numero y cuando llega a 0 te muestra el juego
 		mostrarJuego();
 	}
 	document.getElementById("cuenta").innerHTML=i;
 	i --;
 	setTimeout(Carga, 500);
 }
-
+//lo mismo que Carga() pero mas rapido y intercambia todas las imagenes de los 15 patos por la cara de un participande tel equipo(Tenemos su consetimiento para hacerlo)
 function CargaHard(){	
 	document.getElementById("modoNormal").style.display="none";
 	document.getElementById("modoHard").style.display="none";
@@ -53,34 +55,32 @@ function CargaHard(){
 	document.getElementById("pato14").src="Imagenes/JoanNegro.png";
 	document.getElementById("pato15").src="Imagenes/JoanNegro.png";
 	document.getElementById("juego").style.backgroundColor="brown";
-	tiempomili=500;
-	tiempomilireloj=200;
-	if(i==0){
+	tiempomili=500; //cambia la variable para que el reloj avance mas rapido, cada medio segundo
+	tiempomilireloj=200; // cambia la variable para que las cabezas aparezcan cada 200 milisegundos
+	if(i==0){  //otro sencillo reloj pero en vez de 500 milisegundos son 200
 		mostrarJuego();
 	}
 	document.getElementById("cuenta").innerHTML=i;
 	i --;
 	setTimeout(CargaHard, 200);
 }
-
+//Lo mismo que Carga() y CargaHard() pero mas lento y haciendo que el fondo sea rosa para una mayos sensacion de tranquilidad
 function CargaFancy(){
 	document.getElementById("modoNormal").style.display="none";
 	document.getElementById("modoHard").style.display="none";
 	document.getElementById("modoFancy").style.display="none";
 	document.getElementById("seleccion").innerHTML=":D";
 	document.getElementById("juego").style.backgroundColor="pink";
-	
-	tiempomili=4000;
-	tiempomilireloj=1500;
-	if(i==0){
+	tiempomili=4000; //cambia la variable para que el reloj avance mas lento, cada 4 segundos
+	tiempomilireloj=1500; //cambia la variable para que los patos aparezcan cada 1,5 segundos
+	if(i==0){ //otro reloj sencillo pero esta vez tardara 1,5 segundos en avanzar cada numero para dar mayor sensacion se seguridad
 		mostrarJuego();
 	}
 	document.getElementById("cuenta").innerHTML=i;
 	i --;
 	setTimeout(CargaFancy, 1500);
 }
-
-
+// Funcion que actualiza un reloj y que cnd llega a -1 ejecuta la funcion final()
 function reloj(){
 	if(Reloj==-1){
 		final();
@@ -89,105 +89,103 @@ function reloj(){
 	Reloj--;
 	setTimeout(reloj, tiempomilireloj);
 }
-
+// Funcion que se activa despues de la pantalla de carga
 function mostrarJuego(){
 	reloj();
-	document.getElementById("espera").style.display="none";
-	document.getElementById("Fallos").innerHTML="Fallos: "+fallos;
-	document.getElementById("Balas").innerHTML="Balas: "+balas;
-	document.getElementById("Puntos").innerHTML="Puntos: "+puntos;
-	document.getElementById("juego").style.display="block";
-	document.getElementById("menu").style.display="block";
-	volverMira();
-	volarPatos();
-
+	document.getElementById("espera").style.display="none"; //Oculta la pantalla de carga
+	document.getElementById("Fallos").innerHTML="Fallos: "+fallos; //Muestra por primera vez los fallos
+	document.getElementById("Balas").innerHTML="Balas: "+balas; //Muestra por primera vez las balas
+	document.getElementById("Puntos").innerHTML="Puntos: "+puntos; //Muestra por primera vez los puntos
+	document.getElementById("juego").style.display="block"; //Muestra la pantalla del juego
+	document.getElementById("menu").style.display="block"; //Muestra el menu
+	volverMira(); //pone el cursor en el div juego con una mirilla personalizada creada por nosotros
+	volarPatos(); //ejecuta la funcion que hace que empiezen a salir los patos
 }
-
+// Esta funcion se activa cuando el reloj llega a -1
 function final(){
-	document.getElementById("juego").style.display="none";
-	document.getElementById("menu").style.display="none";
-	document.getElementById("final").style.display="block";
-	document.getElementById("textoFinal").innerHTML="TIEMPOO!";
-	document.getElementById("puntuacionFinal").innerHTML=puntos+" PatoPuntos ";
-	document.getElementById("falloFinal").innerHTML=fallos+" Fallos ";
+	document.getElementById("juego").style.display="none"; //Oculta el juego
+	document.getElementById("menu").style.display="none"; //Oculta el menu
+	document.getElementById("final").style.display="block"; //Muestra el div final
+	document.getElementById("textoFinal").innerHTML="TIEMPOO!"; //Muestra el texto final
+	document.getElementById("puntuacionFinal").innerHTML=puntos+" PatoPuntos "; //Muestra la puntuacion total
+	document.getElementById("falloFinal").innerHTML=fallos+" Fallos "; //Muestra los fallos totales
 }
-
+//Se activa al darle click a los patos normales y suma la constante de los patos normales 
 function sumarPuntosN(){
 	if(balas!=0){
 		puntos += PATO_NORMAL;
 		document.getElementById("matar").volume=1;
-		document.getElementById("matar").play();
+		document.getElementById("matar").play(); //reproduce un sonido especifico
 		document.getElementById("Puntos").innerHTML="Puntos: "+puntos;
 	}
 }
-
+//Se activa al darle click a los patos especiales y suma la constante de los patos especiales
 function sumarPuntosE(){
 	puntos += PATO_ESPECIAL;
 	document.getElementById("matarEspecial").volume=1;
-	document.getElementById("matarEspecial").play();
+	document.getElementById("matarEspecial").play(); //reproduce un sonido especifico
 	document.getElementById("Puntos").innerHTML="Puntos: "+puntos;
 }
-
+//Se activa al darle click a los patos negros y suma la constante de los patos negros
 function restarPuntos(){
 	puntos -= PATO_NEGRO;
 	document.getElementById("matarNegro").volume=1;
-	document.getElementById("matarNegro").play();
+	document.getElementById("matarNegro").play(); //reproduce un sonido especifico
 	document.getElementById("Puntos").innerHTML="Puntos: "+puntos;
 }
-
+//Se acitva al darle click en algun pato o en el fondo
 function disparar(){
 	if(balas==1){
-		recargar();
+		recargar(); //cuando las balas estan en 1 ejecuta la funcion recargar
 	} else{
-	balas -= 1;
-	document.getElementById("juego").style.cursor="url(Imagenes/Disparo.png) 16 16, auto";
-	document.getElementById("disparo").volume=0.1;
-	document.getElementById("disparo").play();
-	document.getElementById("Balas").innerHTML="Balas: "+balas;
-	setTimeout(volverMira,200);
+	balas -= 1; //va restando valas al contador de balas
+	document.getElementById("juego").style.cursor="url(Imagenes/Disparo.png) 16 16, auto";//cambia el cursor 
+	document.getElementById("disparo").volume=0.1;//pone el volumen del audio bajo
+	document.getElementById("disparo").play();//activa el audio de disparar
+	document.getElementById("Balas").innerHTML="Balas: "+balas;//actualiza el contador de balas
+	setTimeout(volverMira,200);//ejecuta  la funcion volverMira() (que devuelve el cursor a la mira) despues de 200 milisegundos
 	}
 }
 
 function recargar(){
-	balas=6;
-	document.getElementById("juego").style.cursor="url(Imagenes/Recargando.png) 16 16, auto";
-	document.getElementById("Balas").innerHTML="Balas: "+balas;
+	balas=6;//Cambia el contador de balas a 6
+	document.getElementById("juego").style.cursor="url(Imagenes/Recargando.png) 16 16, auto";//cambia el cursos a uno que muestra que estas recargando
+	document.getElementById("Balas").innerHTML="Balas: "+balas;//actualiza el contador de balas
 	document.getElementById("recargar").volume=1;
-	document.getElementById("recargar").play();
-	setTimeout(volverMira,1000);
+	document.getElementById("recargar").play();//reproduce un sonido que muestra que estas recargando
+	setTimeout(volverMira,1000);//ejecuta la funcion de volverMira tras un segundo de espera
 }
-
+//devuelve el cursos a la normalidad
 function volverMira(){
 	document.getElementById("juego").style.cursor="url(Imagenes/Mira.png) 16 16, auto";
 }
-
+//devuelve el color del fondo a la normalidad
 function volverFondo(){
 	document.getElementById("juego").style.backgroundColor="#1E93D6"
 }
-
+//se activa al clicar en el fondo
 function fallo(){
-	if(balas!=0){
+	if(balas!=0){//si las balas no son 0 suma 1 al contador de fallos, lo actualiza y cambia el fondo a rojo
 		fallos ++;
 		document.getElementById("Fallos").innerHTML="Fallos: "+fallos;
 		document.getElementById("juego").style.backgroundColor="red";
-		setTimeout(volverFondo, 100);
+		setTimeout(volverFondo, 100);//devuelve el fondo a su color inicial tras 100 milisegundos
 	}
-	disparar();
+	disparar();//ejecuta la funcion de disparar
 }
-
+//recoge el valor del pato en el que has clikcado(1,2,3,...) y lo oculta
 function ocultar(a){
 	a.style.display="none";
 }
-
-
+//esta activa desde que se va la pantalla de carga
 function volarPatos(){
 	if(puntos>=0){
-		pato=[Math.floor(Math.random()*15)+0];
-		document.getElementById(patos[pato]).style.display="block";
+		pato=[Math.floor(Math.random()*15)+0];//selecciona un pato al azar de la array de patos
+		document.getElementById(patos[pato]).style.display="block";//muestra ese pato, lo que hace que comienze su animacion predefinida en el css
 	}
-	setTimeout(volarPatos,tiempomili);
+	setTimeout(volarPatos,tiempomili);//vuelve a ejecurar esta funcion despues de los segundos selsccionados al elgir la dificultad
 }
-
+//activa o desactiva una cancion en 8 bits sin copyright 
 function Volumen(){
 	if(mute==1){
 		document.getElementById("BMute").src="Imagenes/Muteado.png";
--- Javascript/Juego2.js
+++ Javascript/Juego2.js
@@ -1,56 +1,52 @@
 //VARIABLES
-var suE= Math.floor(Math.random()*3)+1;
-var vic=0;
+var suE= Math.floor(Math.random()*3)+1; //hace que la eleccion del oponente sea totalmente aleatoria
 var i;
 
 
-//METODOS
-function empezar(){
+//FUNCIONES
+function empezar(){//al clickar en la imagen de las espadas cruzadas oculta todo lo perteneciente al menu y muestra la siguiente pantalla
     ocultarMenu();
     document.getElementById("ppt").style.display="block";
     document.getElementById("puntos").style.display="block";
     document.getElementById("puntos").innerHTML="Puntos: "+vic;
 }
 
-function ocultarMenu(){
+function ocultarMenu(){//oculta el logo, el texto y la imagen de las espadas
     document.getElementById("logo").style.display="none";
     document.getElementById("modo").style.display="none";
     document.getElementById("go").style.display="none";
 }
 
-function empate(){
+function empate(){//muestra un texto de "empate" cambia el color de fondo y el color del texto
     document.getElementById("versusp").innerHTML="EMPATE";
     document.getElementById("versusp").style.color="#00e5ff";
     document.getElementById("PPT").style.backgroundColor="blue";
 }
-function ganar(){
+function ganar(){//muestra un texto de "victoria" cambia el color de fondo, el color del texto 
     document.getElementById("versusp").innerHTML="VICTORIA";
     document.getElementById("versusp").style.color="#22f5cb";
     document.getElementById("PPT").style.backgroundColor="green";
-    vic++;
-    document.getElementById("puntos").innerHTML="Puntos: "+vic;
 }
-function perder(){
+function perder(){//muestra un texto de "Derrota" cambia el color de fondo y el color del texto
     document.getElementById("versusp").innerHTML="DERROTA";
     document.getElementById("versusp").style.color="#27032e";
     document.getElementById("PPT").style.backgroundColor="red";
 }
 
-function terminarJuego(){
+function terminarJuego(){//vuelve a mostrar la pantalla inicial y oculta todo lo siguiente a ella
+    document.getElementById("PPT").style.backgroundColor="rgb(158, 33, 127)";
     document.getElementById("logo").style.display="block";
     document.getElementById("modo").style.display="block";
-    document.getElementById("una").style.display="block";
-    document.getElementById("UNA").style.display="block";
-    document.getElementById("tres").style.display="block";
-    document.getElementById("TRES").style.display="block";
+    document.getElementById("go").style.display="block";
     document.getElementById("versus").style.display="none";
+    suE= Math.floor(Math.random()*3)+1;
 }
 
-function quienGana(a){
+function quienGana(a){//funcion que muestra la pantalla de las dos elecciones
     document.getElementById("ppt").style.display="none";
     document.getElementById("versus").style.display="block";
     document.getElementById("versus").style.display="block";
-    if(suE==a){
+    if(suE==a){// condicion para saber si tu eleccion gana, pierde o empata contra la suya
         empate();
     }else if(suE==3 && a==2){
         perder();
@@ -63,34 +59,20 @@ function quienGana(a){
     }else if(suE==1 && a==3){
         perder();
     }else if(suE==1 && a==2){
-           ganar();
+        ganar();
     }
-    if(suE==1){
+    if(suE==1){//condicion para mostrar su eleccion
         document.getElementById("suE").src="Imagenes/PiedraE.png";
     } else if(suE==2){
         document.getElementById("suE").src="Imagenes/PapelE.png"
     } else{
         document.getElementById("suE").src="Imagenes/TijeraE.png"
     }
-    if(a==1){
+    if(a==1){//condicion para mostrar tu eleccion
         document.getElementById("tuE").src="Imagenes/Piedra.png";
     } else if(a==2){
         document.getElementById("tuE").src="Imagenes/Papel.png"
     } else{
         document.getElementById("tuE").src="Imagenes/Tijera.png"     
     }
-    jugarVeces--;
-    if(jugarVeces==3 && vic==2 || jugarVeces==1 && vic==1){
-        document.getElementById("otra").src="Imagenes/victoria.png";
-        terminar=1;
-    }
-}
-
-function vj(){
-    if(terminar==1){
-        terminarJuego();
-    }
-    document.getElementById("versus").style.display="none";
-    document.getElementById("ppt").style.display="block";
-    document.getElementById("otra").style.display="none";
 }
